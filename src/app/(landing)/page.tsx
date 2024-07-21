79c7316ba15e17db325be0ec8645a701c3448bf8
John P <johnrprutton@gmail.com>
2024-07-21 22:31:11 +0200

    initial styling

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+
+import style from "./style.module.css"
+
+export default function Home() {
+  return (
+    <main className={style["radial-bg"]}>
+      <div className="w-fit max-w-md">
+        <span className="text-5xl font-black">ChequeMate</span>
+
+        <h1 className="mt-8 text-xl tracking-wide">
+          Take a picture of your bill and split it among friends!
+        </h1>
+
+        <Button asChild className={style["animate-small-ping"]}>
+          <Link href="/new">Get Started</Link>
+        </Button>
+      </div>
+    </main>
+  )
+}
