fff8e36ccf2a7b9009d8207215810046f5d4764e
John P <johnrprutton@gmail.com>
2024-07-24 09:22:31 +0200

    fix: update paths to match new folder structure

---
+++ src/infrastructure/db/drizzle.config.ts
@@ -0,0 +1,16 @@
+import { join } from "path"
+
+import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+config({ path: ".env.local" })
+
+export default defineConfig({
+  schema: join(__dirname, "schema.ts").replaceAll("\\", "/"),
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!
+  },
+  verbose: true,
+  strict: true
+})
